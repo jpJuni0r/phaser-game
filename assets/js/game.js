318fa84df37afcbaf055c0e681492257fbcfe35f
Jan-Philipp <post.janph@gmail.com>
2015-12-05 16:20:40 +0100

    Added ground

--- assets/js/game.js
+++ assets/js/game.js
@@ -8,9 +8,15 @@ PhaserGame.prototype = {
   preload: function() {
     this.load.image('background', 'assets/img/background-full.png');
     this.load.image('pipe', 'assets/img/pipe.png');
+    this.load.image('ground', 'assets/img/ground.png');
+    this.load.spritesheet('player', 'assets/img/player.png');
   },
   create: function() {
     game.add.image(0, 0, 'background');
+
+    // Physics
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+    game.physics.arcade.gravity.y = 400
 
 
     // Adding Pipes
@@ -19,6 +25,14 @@ PhaserGame.prototype = {
 
     var rightPipeSprite = game.add.sprite(568, 352 + 32, 'pipe');
     rightPipeSprite.scale.setTo(2, 2);
+
+
+    // Ground
+    var ground = [];
+    for (var i = 0; i < 20; i++) {
+      ground.push(game.add.sprite(32 * i, 448, 'ground'));
+      ground[i].scale.setTo(2, 2);
+    }
   }
 }
 
